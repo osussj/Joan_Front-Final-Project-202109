2c9008496173a0a13ff64fe8b4dcdb2205d6075c
osu <joanosuna9@gmail.com>
2021-11-29 10:22:55 +0100

    Added initial config of Angular

---
+++ src/app/escroom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./escroom.component.html",
+  styleUrls: ["./escroom.component.css"],
+})
+export class AppComponent {
+  title = "escroomfront";
+}
