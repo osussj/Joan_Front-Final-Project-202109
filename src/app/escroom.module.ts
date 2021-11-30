ac8d4413f63c831e240f884ae1c82071b43314c9
osu <joanosuna9@gmail.com>
2021-11-30 21:24:09 +0100

    Added navbar to roomcomponent

--- src/app/escroom.module.ts
+++ src/app/escroom.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { HttpClientModule } from "@angular/common/http";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AppRoutingModule } from "./escroom-routing.module";
 import { AppComponent } from "./escroom.component";
+import { RoomService } from "./core/services/room/room.service";
+import { RoomComponent } from "./components/room/room.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, RoomComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FontAwesomeModule,
+  ],
+  providers: [RoomService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
