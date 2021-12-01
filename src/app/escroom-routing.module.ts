f0ac93e7b65cfe49d42b2e62b0a6460949653d82
osu <joanosuna9@gmail.com>
2021-12-01 17:17:21 +0100

    Added GuardService to routing module

--- src/app/escroom-routing.module.ts
+++ src/app/escroom-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RoomComponent } from "./components/room/room.component";
 import { SigninComponent } from "./components/signin/signin.component";
+import { GuardService } from "./core/services/guard/guard.service";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/", pathMatch: "full" },
   { path: "signin", component: SigninComponent },
-  { path: "dashboard", component: RoomComponent },
+  { path: "dashboard", component: RoomComponent, canActivate: [GuardService] },
+  { path: "**", redirectTo: "/dashboard", pathMatch: "full" },
 ];
 
 @NgModule({
