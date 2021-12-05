96708b546fed7cc8c22318b353d66e51444bbba7
osu <joanosuna9@gmail.com>
2021-12-06 00:47:27 +0100

    Added Myplacehomecomponent

---
+++ src/app/components/myplacehome/myplacehome.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "escroom-myplacehome",
+  templateUrl: "./myplacehome.component.html",
+  styleUrls: ["./myplacehome.component.css"],
+})
+export class MyplacehomeComponent implements OnInit {
+  ngOnInit(): void {}
+}
