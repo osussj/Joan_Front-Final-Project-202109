5f93db7f87d616407b21cd26b0579bb6db5da384
osu <joanosuna9@gmail.com>
2021-12-05 17:01:11 +0100

    Added signin component test

--- src/app/components/signin/signin.component.spec.ts
+++ src/app/components/signin/signin.component.spec.ts
@@ -1,5 +1,51 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { RouterTestingModule } from "@angular/router/testing";
+import { SigninComponent } from "./signin.component";
+
 describe("SigninComponent", () => {
+  let component: SigninComponent;
+  let fixture: ComponentFixture<SigninComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      declarations: [SigninComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SigninComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it("should create", () => {
-    expect(null).toBeFalsy();
+    expect(component).toBeTruthy();
+  });
+  it("should be valid when the user fills in all the inputs", () => {
+    const usernameInput =
+      fixture.debugElement.nativeElement.querySelectorAll("input")[0];
+    const passwordInput =
+      fixture.debugElement.nativeElement.querySelectorAll("input")[1];
+    const submitButton = fixture.debugElement.query(By.css("form"));
+    spyOn(component, "onSubmit");
+
+    usernameInput.value = "guest";
+    passwordInput.value = "tseug";
+
+    usernameInput.dispatchEvent(new Event("input"));
+    passwordInput.dispatchEvent(new Event("input"));
+    submitButton.triggerEventHandler("submit", null);
+    fixture.detectChanges();
+
+    expect(component.onSubmit).toHaveBeenCalledTimes(1);
   });
 });
