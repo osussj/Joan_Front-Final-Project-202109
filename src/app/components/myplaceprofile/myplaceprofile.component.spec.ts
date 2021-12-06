751cad23d7577f93b54be2c0324101f072c85307
osu <joanosuna9@gmail.com>
2021-12-06 21:23:04 +0100

    Added MyPlace component test

---
+++ src/app/components/myplaceprofile/myplaceprofile.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { MyplaceprofileComponent } from "./myplaceprofile.component";
+
+describe("MyplaceprofileComponent", () => {
+  let component: MyplaceprofileComponent;
+  let fixture: ComponentFixture<MyplaceprofileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MyplaceprofileComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyplaceprofileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+  it("should render tom title", () => {
+    const expectedText = "mark";
+    const userTitle = fixture.debugElement.nativeElement.querySelector("h2");
+    expect(userTitle.textContent).toContain(expectedText);
+  });
+  it("should render MYPLACE title", () => {
+    const expectedText = "WELCOME TO MY PLACE";
+    const userTitle = fixture.debugElement.nativeElement.querySelector("h1");
+    expect(userTitle.textContent).toContain(expectedText);
+  });
+});
