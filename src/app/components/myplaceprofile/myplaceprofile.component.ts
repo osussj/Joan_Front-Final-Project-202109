80760229ee42c742a4c7cb6e13b037a73e986881
osu <joanosuna9@gmail.com>
2021-12-06 21:22:49 +0100

    Added MyPlace component

---
+++ src/app/components/myplaceprofile/myplaceprofile.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "escroom-myplaceprofile",
+  templateUrl: "./myplaceprofile.component.html",
+  styleUrls: ["./myplaceprofile.component.css"],
+})
+export class MyplaceprofileComponent implements OnInit {
+  ngOnInit(): void {}
+}
