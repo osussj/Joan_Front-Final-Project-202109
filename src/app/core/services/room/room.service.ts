8925d2c1387cae0a009cf7ccdaef4af27050a122
osu <joanosuna9@gmail.com>
2021-12-06 17:55:53 +0100

    Added updateQuestion method to roomService

--- src/app/core/services/room/room.service.ts
+++ src/app/core/services/room/room.service.ts
@@ -7,50 +7,66 @@ import { IQuestion } from "../../models/question";
   providedIn: "root",
 })
 export class RoomService {
-  apiUrl: string =
-    "https://proyecto-final-joan-back.herokuapp.com/api/room/node";
+  apiUrl: string = "https://escroom.herokuapp.com/api/room/node";
+
+  getToken() {
+    const userLogged = localStorage.getItem("user")
+      ? JSON.parse(localStorage.getItem("user") || "")
+      : "";
+
+    if (userLogged) {
+      return userLogged.token;
+    }
+    return null;
+  }
 
   constructor(private http: HttpClient) {}
 
   loadRoom(): Observable<any> {
+    const token = this.getToken();
     return this.http.get(this.apiUrl, {
       headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem("user") || "").token
-        }`,
+        Authorization: `Bearer ${token}`,
       },
     });
   }
 
   loadQuestions(): Observable<any> {
+    const token = this.getToken();
     return this.http.get(`${this.apiUrl}/question`, {
       headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem("user") || "").token
-        }`,
+        Authorization: `Bearer ${token}`,
       },
     });
   }
 
   postQuestion(data: IQuestion): Observable<any> {
+    const token = this.getToken();
     return this.http.post(`${this.apiUrl}/question`, data, {
       headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem("user") || "").token
-        }`,
+        Authorization: `Bearer ${token}`,
       },
     });
   }
 
   deleteQuestion(id: string): Observable<any> {
+    const token = this.getToken();
     return this.http.request("delete", `${this.apiUrl}/question`, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem("user") || "").token
-        }`,
+        Authorization: `Bearer ${token}`,
       },
       body: { id },
+    });
+  }
+
+  updateQuestion(data: object): Observable<any> {
+    const token = this.getToken();
+    return this.http.request("put", `${this.apiUrl}/question`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: data,
     });
   }
 }
