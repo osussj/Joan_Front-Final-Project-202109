75a96f5e1a35ea7be6dd0613796d2eee4c404725
osu <joanosuna9@gmail.com>
2021-12-08 00:12:56 +0100

    Added Register component

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { StoreService } from "src/app/core/services/store/store.service";
+
+@Component({
+  selector: "escroom-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
+})
+export class RegisterComponent implements OnInit {
+  registerForm: any | FormGroup;
+
+  errorMessage: string = "";
+
+  constructor(
+    public storeService: StoreService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      username: ["", Validators.required],
+      email: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.storeService.registerUser(this.registerForm.value).subscribe({
+      next: () => {
+        this.router.navigate(["/login"]);
+      },
+      error: (e) => {
+        const loginError = e.error.error;
+        this.errorMessage = loginError;
+      },
+    });
+  }
+}
