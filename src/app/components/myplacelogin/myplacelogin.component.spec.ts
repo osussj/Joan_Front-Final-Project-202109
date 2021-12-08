ec7878341c3a5115fa4a02a267a55d6860589d16
osu <joanosuna9@gmail.com>
2021-12-08 11:04:23 +0100

    Added MyPlaceLogin component test

--- src/app/components/myplacelogin/myplacelogin.component.spec.ts
+++ src/app/components/myplacelogin/myplacelogin.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { By } from "@angular/platform-browser";
 import { RouterTestingModule } from "@angular/router/testing";
+import { ToastrService } from "ngx-toastr";
+import { Observable } from "rxjs";
+import { StoreService } from "src/app/core/services/store/store.service";
 import { MyplacenavbarComponent } from "../myplacenavbar/myplacenavbar.component";
 
 import { MyplaceloginComponent } from "./myplacelogin.component";
@@ -10,6 +13,20 @@ import { MyplaceloginComponent } from "./myplacelogin.component";
 describe("MyplaceloginComponent", () => {
   let component: MyplaceloginComponent;
   let fixture: ComponentFixture<MyplaceloginComponent>;
+  let storeService: StoreService;
+
+  const ToastrServiceMock = {
+    success: () => {},
+    error: () => {},
+  };
+
+  const StoreServiceMock = {
+    loginUserNode: () =>
+      new Observable((observer) => {
+        observer.next(component.router.navigate(["/myplace/admin"]));
+        observer.error("Hi");
+      }),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,7 +34,19 @@ describe("MyplaceloginComponent", () => {
         HttpClientTestingModule,
         FormsModule,
         ReactiveFormsModule,
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: "myplace/admin", component: MyplaceloginComponent },
+        ]),
+      ],
+      providers: [
+        {
+          provide: StoreService,
+          useValue: StoreServiceMock,
+        },
+        {
+          provide: ToastrService,
+          useValue: ToastrServiceMock,
+        },
       ],
       declarations: [MyplaceloginComponent, MyplacenavbarComponent],
     }).compileComponents();
@@ -26,6 +55,7 @@ describe("MyplaceloginComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MyplaceloginComponent);
     component = fixture.componentInstance;
+    storeService = TestBed.inject(StoreService);
     fixture.detectChanges();
   });
 
@@ -38,12 +68,12 @@ describe("MyplaceloginComponent", () => {
     expect(submitButton.nativeElement.disabled).toBeTrue();
   });
   it("should be valid when the user fills in all the inputs", () => {
+    const spyFn = spyOn(storeService, "loginUserNode").and.callThrough();
     const usernameInput =
       fixture.debugElement.nativeElement.querySelectorAll("input")[0];
     const passwordInput =
       fixture.debugElement.nativeElement.querySelectorAll("input")[1];
     const submitButton = fixture.debugElement.query(By.css("form"));
-    spyOn(component, "onSubmit");
 
     usernameInput.value = "guest";
     passwordInput.value = "tseug";
@@ -51,8 +81,19 @@ describe("MyplaceloginComponent", () => {
     usernameInput.dispatchEvent(new Event("input"));
     passwordInput.dispatchEvent(new Event("input"));
     submitButton.triggerEventHandler("submit", null);
+
     fixture.detectChanges();
 
-    expect(component.onSubmit).toHaveBeenCalledTimes(1);
+    expect(spyFn).toHaveBeenCalled();
+  });
+  it("should render WELCOME TO MY PLACE title", () => {
+    const expectedText = "WELCOME TO MY PLACE";
+    const mainTitle = fixture.debugElement.nativeElement.querySelector("h1");
+    expect(mainTitle.textContent).toContain(expectedText);
+  });
+  it("should render LOGIN title", () => {
+    const expectedText = "LOGIN";
+    const loginTitle = fixture.debugElement.nativeElement.querySelector("h2");
+    expect(loginTitle.textContent).toContain(expectedText);
   });
 });
