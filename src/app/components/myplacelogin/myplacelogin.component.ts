658432704abbb58e92fca6e3d8d792f91b5dd4dd
osu <joanosuna9@gmail.com>
2021-12-08 01:20:23 +0100

    Added MyPlaceLogin component

---
+++ src/app/components/myplacelogin/myplacelogin.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { StoreService } from "src/app/core/services/store/store.service";
+
+@Component({
+  selector: "escroom-myplacelogin",
+  templateUrl: "./myplacelogin.component.html",
+  styleUrls: ["./myplacelogin.component.css"],
+})
+export class MyplaceloginComponent implements OnInit {
+  loginForm: any | FormBuilder;
+
+  errorMessage: string = "";
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public storeService: StoreService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.storeService.loginUserNode(this.loginForm.value).subscribe({
+      next: () => {
+        this.router.navigate(["/myplace/admin"]);
+      },
+    });
+  }
+}
